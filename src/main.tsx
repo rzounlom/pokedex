e94f0ee3df88ac8dc8179a06d47132d194425662
Romaric Zounlome <rzounlom@gmail.com>
2024-09-19 07:59:11 -0400

    configure bootstrap/react-toastify; complete homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import { StrictMode } from "react";
+import { ToastContainer } from "react-toastify";
 import { createRoot } from "react-dom/client";
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
     <RouterProvider router={router} />{" "}
   </StrictMode>
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
+import "./HomePage.css";
+
 import { FC } from "react";
 
 const HomePage: FC = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="homepage">
+      <div className="bg-container">
+        <div className="heading">
+          <h1>Welcome to Official Pokédemon Fan Page</h1>
+          <button>View Pokédemon</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
